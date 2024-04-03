ce7b99b1bfcf84faa03db2e8ba736f2fa3cb4519
Kota Iijima <kota@MacBook-Air.local>
2024-04-03 20:22:00 +0900

    formの実装

--- app/decorators/user_lifeplan_decorator.rb
+++ app/decorators/user_lifeplan_decorator.rb
@@ -1,29 +1,32 @@
 class UserLifeplanDecorator < ApplicationDecorator
   delegate_all
 
-  def form_objects(f)
-    [
+  class << self
+    def form_objects(f)
+      p 'form_objects'
       [
-        init_form(f,
-                  { code: :name, input: f.text_field(:name, class: f.object.decorate.input_class(:name, :admin), placeholder: ''),
-                    col: 6, no_required: false, help: '', alert: '' }),
-      ],
-      [
-        init_form(f,
-                  { code: :status, input: f.text_field(:status, class: f.object.decorate.input_class(:status, :admin), placeholder: ''),
-                    col: 6, no_required: false, help: '', alert: '' }),
-      ],
-      [
-        init_form(f,
-                  { code: :apply_reviewed_at, input: f.text_field(:apply_reviewed_at, class: f.object.decorate.input_class(:apply_reviewed_at, :admin), placeholder: ''),
-                    col: 6, no_required: false, help: '', alert: '' }),
-      ],
-      [
-        init_form(f,
-                  { code: :reviewed_at, input: f.text_field(:reviewed_at, class: f.object.decorate.input_class(:reviewed_at, :admin), placeholder: ''),
-                    col: 6, no_required: false, help: '', alert: '' }),
-      ],
-    ]
+        [
+          init_form(f,
+                    { code: :name, input: f.text_field(:name, class: f.object.decorate.input_class(:name, :admin), placeholder: ''),
+                      col: 6, no_required: false, help: '', alert: '' }),
+        ],
+        [
+          init_form(f,
+                    { code: :status, input: f.text_field(:status, class: f.object.decorate.input_class(:status, :admin), placeholder: ''),
+                      col: 6, no_required: false, help: '', alert: '' }),
+        ],
+        [
+          init_form(f,
+                    { code: :apply_reviewed_at, input: f.text_field(:apply_reviewed_at, class: f.object.decorate.input_class(:apply_reviewed_at, :admin), placeholder: ''),
+                      col: 6, no_required: false, help: '', alert: '' }),
+        ],
+        [
+          init_form(f,
+                    { code: :reviewed_at, input: f.text_field(:reviewed_at, class: f.object.decorate.input_class(:reviewed_at, :admin), placeholder: ''),
+                      col: 6, no_required: false, help: '', alert: '' }),
+        ],
+      ]
+    end
   end
 
 end
