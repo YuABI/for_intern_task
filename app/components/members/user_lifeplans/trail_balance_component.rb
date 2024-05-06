bcc1bc8623f6490b4e30d5fbb292a42e694b4cd1
Kota Iijima <kota@MacBook-Air.local>
2024-05-06 18:11:14 +0900

    試算表の実装

--- app/components/members/user_lifeplans/trail_balance_component.rb
+++ app/components/members/user_lifeplans/trail_balance_component.rb
@@ -10,4 +10,19 @@ class Members::UserLifeplans::TrailBalanceComponent < Members::BaseComponent
     @user_lifeplan_decorator = args[:user_lifeplan_decorator]
     @user_lifeplan_yearly_blance = args[:user_lifeplan_yearly_blance]
   end
+
+  def sum_each_amount(*arg)
+    total = 0
+    arg.each do |each_amount|
+      total = total + each_amount
+    end
+
+    total
+  end
+
+  def change_delimited(number)
+    return '-' if number.blank? || number.zero?
+
+    number_to_currency(number, unit: '', precision: 0)
+  end
 end
